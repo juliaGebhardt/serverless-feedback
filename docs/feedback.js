aaaabd9db98bf98732bc11427a715b9d006f52fe
Julia Mende <julia.gebhardt@de.ibm.com>
2019-03-29 15:05:14 +0100

    api edited

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/feedback';
+const apiUrl = 'https://3dd89b46.eu-gb.apiconnect.appdomain.cloud/31-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
